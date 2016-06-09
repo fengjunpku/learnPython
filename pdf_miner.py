935c353d47b77364b7bb492e2858c67dd32507f2
fengjun <fengjun2013@pku.edu.cn>
2016-06-10 01:08:11 +0800

    pdfminer

--- pdf_miner.py
+++ pdf_miner.py
@@ -1,10 +1,10 @@
-from urllib import urlopen
+from urllib2 import Request
+from urllib2 import urlopen
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-from io import StringIO
-from io import open
+from cStringIO import StringIO
 
 def readPDF(pdfFile):
   rsrcmgr = PDFResourceManager()
@@ -23,7 +23,32 @@ def readPDF(pdfFile):
   retstr.close()
   return content
 
-pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf");
+#pdfFile = urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf");
 #outputString = readPDF(pdfFile)
-print(pdfFile)
-pdfFile.close()
+# print(pdfFile)
+# pdfFile.close()
+
+if __name__ == "__main__":
+  url = 'http://pythonscraping.com/pages/warandpeace/chapter1.pdf'
+  open = urlopen(Request(url)).read()
+  memoryFile = StringIO(open)
+  #print memoryFile
+  rsrcmgr = PDFResourceManager()
+  retstr = StringIO()
+  codec = 'utf-8'
+  laparams = LAParams()
+  device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+  fp = file('chapter1.pdf', 'rb')
+  print fp
+  interpreter = PDFPageInterpreter(rsrcmgr, device)
+  password = ""
+  maxpages = 0
+  caching = True
+  pagenos=set()
+  for page in PDFPage.get_pages(memoryFile, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+    interpreter.process_page(page)
+  fp.close()
+  device.close()
+  str = retstr.getvalue()
+  retstr.close()
+  print str
