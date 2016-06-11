8ca7f4a948edd37178ff7bac65291f829167c0d4
fengjunpku <fengjun2013@pku.edu.cn>
2016-06-11 22:11:51 +0800

    pyRoot

---
+++ rootpy/root.py
@@ -0,0 +1,12 @@
+#!/home/fengjun/bin/py27
+import ROOT
+# Example: displaying a ROOT histogram from Python
+from ROOT import gRandom,TCanvas,TH1F
+c1 = TCanvas('c1','Example',200,10,700,500)
+hpx = TH1F('hpx','px',100,-4,4)
+for i in xrange(25000):
+	px = gRandom.Gaus()
+	hpx.Fill(px)
+	hpx.Draw()
+	c1.Update()
+
