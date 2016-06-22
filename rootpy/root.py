262cf33d9ee304e93a219ce03dd3cd892f505832
fengjunpku <fengjun2013@pku.edu.cn>
2016-06-22 14:44:08 +0800

    little

--- rootpy/root.py
+++ rootpy/root.py
@@ -3,9 +3,9 @@ import ROOT
 # Example: displaying a ROOT histogram from Python
 from ROOT import gRandom,TCanvas,TH1F
 c1 = TCanvas('c1','Example',200,10,700,500)
-hpx = TH1F('hpx','px',100,-4,4)
+hpx = TH1F('hpx','px',200,-10,10)
 for i in xrange(25000):
-	px = gRandom.Gaus()
+	px = gRandom.Gaus(0,1)
 	hpx.Fill(px)
 	hpx.Draw()
 	c1.Update()
