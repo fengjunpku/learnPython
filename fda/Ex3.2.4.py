888894092281cd4cb0a9f124d301eb575bcb1300
fengjunpku <fengjun2013@pku.edu.cn>
2016-06-23 19:20:06 +0800

    little

---
+++ fda/Ex3.2.4.py
@@ -0,0 +1,27 @@
+#python27
+import math
+from numpy import *
+import matplotlib.pyplot as plt
+from time import time
+
+random.seed(20000)
+t0 = time()
+#parameters
+S0 = 100
+K = 105
+T = 1.0
+r = 0.05
+sigma = 0.2
+M = 50
+dt = T/M
+I = 250000
+# simu I path
+S = S0*exp(cumsum((r-0.5*sigma**2)*dt+sigma*math.sqrt(dt)*random.standard_normal((M+1,I)),axis=0))
+
+S[0]=S0
+C0 = math.exp(-r*T)*sum(maximum(S[-1]-K,0))/I
+tnp2 = time()-t0
+#print C0
+#print tnp2
+plt.plot(S[:,:10])
+plt.show()
