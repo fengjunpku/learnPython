465a8235b0653497e1dafc36d839cfcda9174c48
fengjunpku <fengjun2013@pku.edu.cn>
2016-06-24 18:48:26 +0800

    little

--- rootpy/root.py
+++ rootpy/root.py
@@ -5,8 +5,8 @@ from ROOT import gRandom,TCanvas,TH1F
 c1 = TCanvas('c1','Example',200,10,700,500)
 hpx = TH1F('hpx','px',200,-10,10)
 for i in xrange(25000):
-	px = gRandom.Gaus(0,1)
-	hpx.Fill(px)
-	hpx.Draw()
-	c1.Update()
+  px = gRandom.Gaus(0,1)
+  hpx.Fill(px)
+  hpx.Draw()
+  c1.Update()
 
