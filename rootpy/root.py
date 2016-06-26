5531667dcf7893c9ab6370dc90b82cc829b68158
fengjunpku <fengjun2013@pku.edu.cn>
2016-06-26 12:44:07 +0800

    pyroot in os X

--- rootpy/root.py
+++ rootpy/root.py
@@ -1,12 +1,15 @@
-#!/home/fengjun/bin/py27
+#!/usr/bin/python
 import ROOT
 # Example: displaying a ROOT histogram from Python
-from ROOT import gRandom,TCanvas,TH1F
+from ROOT import gRandom,TCanvas,TH1F,gSystem
 c1 = TCanvas('c1','Example',200,10,700,500)
 hpx = TH1F('hpx','px',200,-10,10)
 for i in xrange(25000):
   px = gRandom.Gaus(0,1)
+  #print px
   hpx.Fill(px)
   hpx.Draw()
   c1.Update()
-
+  c1.Modified()
+  if gSystem.ProcessEvents():
+    break
