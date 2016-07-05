6ef9ac66d786b1f03f2d65d907900102b9f2e06e
fengjunpku <fengjun2013@pku.edu.cn>
2016-07-06 00:42:31 +0800

    tushare

--- fda/README.md
+++ fda/README.md
@@ -1,2 +1,4 @@
 # Discription
 学习金融中的数据处理
+# History
+2016-07-05 国人开发开源财经数据接口`tushare`
---
+++ fda/tutest.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+import tushare as ts
+print ts.get_hist_data('600848',start='2015-01-05',end='2015-01-09')
