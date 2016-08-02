d0d2ec73d69c94c2f78847441b51e9173a8ba715
fengjunpku <fengjun2013@pku.edu.cn>
2016-08-02 19:11:42 +0800

    mac speach

--- autogui/sound.py
+++ autogui/sound.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-import time,sys
+import time,sys,os
 def beep():
 	i = 0
-	while i<100:
+	while i<10:
 		sys.stdout.write('\a\n')
 		#print '\a'
 		time.sleep(0.1)
@@ -10,3 +10,4 @@ def beep():
 
 if __name__ == "__main__":
 	beep()
+	os.system('say "your program has finished"')
